b46b3926831e45cc2cd4f27e4570035e5f8083b7
yogesh kannan <yogeshkannan.nk@gmail.com>
2026-04-13 11:11:13 +0530

    Create sum2Num.py

---
+++ simple/sum2Num.py
@@ -0,0 +1,7 @@
+#sum of 2 numbers with constraint
+a=int(input("Enter a number :"))
+b=int(input("Enter a number :"))
+if(0<=a<=1000 and 0<=b<=1000):
+   print("Sum :",a+b)
+else:
+   print("Invalid input")
