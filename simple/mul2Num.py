527e96efd762c8a20aba32243d434a1ae98a07a5
yogesh kannan <yogeshkannan.nk@gmail.com>
2026-04-13 11:21:31 +0530

    Create mul2Num.py

---
+++ simple/mul2Num.py
@@ -0,0 +1,7 @@
+#Mul of 2 numbers with constraint
+a=int(input("Enter a number :"))
+b=int(input("Enter a number :"))
+if(0<=a<=1000 and 0<=b<=1000):
+   print("Mul :",a*b)
+else:
+   print("Invalid input")
